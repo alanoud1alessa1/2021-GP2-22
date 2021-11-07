86c18d34eb9c0996090d3e7f9820e94d23e2809b
NoufAlDuraibi <78146394+NoufAlDuraibi@users.noreply.github.com>
2021-11-08 01:27:24 +0300

    Sprint1_submission

--- Filmey-GP1/server/setupTest.js
+++
@@ -1,7 +0,0 @@
-module.exports = async () => {
-  delete require.cache[require.resolve("./db.js")];
-  let db = require("./db");
-  await db.migrate.rollback();
-  await db.migrate.latest();
-  await db.seed.run();
-};
--- Filmey-GP1/server/teardown.js
+++
@@ -1,5 +0,0 @@
-const db = require("./db");
-
-module.exports = async () => {
-  await db.destroy();
-};
